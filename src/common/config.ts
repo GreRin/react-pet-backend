6969b47ac0006302b7f750aefc58f2621b01c7da
GreRin <rigor1988@gmail.com>
2021-11-05 21:13:27 +0300

    Start sever with TypeORM

---
+++ src/common/config.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+
+import path from 'path';
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+type iDbType = 'mssql' | 'postgres' | 'mysql' | 'oracle' | 'mongodb';
+
+export interface ProcessEnv {
+  PORT: number;
+  NODE_ENV: number;
+  JWT_SECRET_KEY: string;
+  AUTH_MODE: string;
+  LOG_FOLDER: string;
+
+  DB_TYPE: iDbType;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
+}
+
+declare const process: {
+  env: ProcessEnv;
+};
+
+export const {
+  PORT,
+  NODE_ENV,
+  JWT_SECRET_KEY,
+  LOG_FOLDER,
+
+  DB_TYPE,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+} = process.env;
+
+export const AUTH_MODE = process.env.AUTH_MODE === 'true';
---
+++ src/common/ormconfig.ts
@@ -0,0 +1,22 @@
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+// import { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from './config';
+//
+// export const config: TypeOrmModuleOptions = {
+//    type: DB_TYPE,
+//    host: DB_HOST,
+//    port: Number(DB_PORT),
+//    username: DB_USER,
+//    password: DB_PASSWORD,
+//    database: DB_NAME,
+//    synchronize: true,
+//    logging: false,
+//    entities: ["src/entity/**/*.ts"],
+//    migrations: ["src/migration/**/*.ts"],
+//    cli: {
+//       entitiesDir: "src/entity",
+//       migrationsDir: "src/migration",
+//       subscribersDir: "src/subscriber"
+//    }
+// };
+//
+// export default config;
