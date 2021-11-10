5d42c3d8f18a9fb9ef20b46aba94a68bf0f40ca2
GreRin <rigor1988@gmail.com>
2021-11-10 16:59:18 +0300

    fixed linter bugs

--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -3,8 +3,8 @@ import { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from './conf
 export const config: {
   cli: { subscribersDir: string; entitiesDir: string; migrationsDir: string };
   secretArn: string; type: string; password: string; database: string; driver: undefined; port: number;
-  entities: string[]; migrations: string[]; host: string; location: string; resourceArn: string;
-  region: string; username: string; synchronize: boolean
+  entities: string[]; migrations: string[]; host: string; location: string; resourceArn: string; region:
+  string; username: string; synchronize: boolean
 } = {
   driver: undefined, location: "", region: "", resourceArn: "", secretArn: "",
   type: DB_TYPE,
