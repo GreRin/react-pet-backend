d5ec5fb7a7aef90063dc99f5e3b26e2d70a2b221
GreRin <rigor1988@gmail.com>
2021-11-11 19:17:24 +0300

    Used async/await construction in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { PORT } from './common/config';
-import { connectToDB } from './helpers/db';
+import { PORT } from "./common/config";
+import { connectToDB } from "./helpers/db";
 
-import app from './app';
+import app from "./app";
 
-connectToDB()
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(`App is running on http://localhost:${PORT}`)
-    );
-  })
-  .catch((err: Error) => {
-    console.log(`Fail connect to DB: `, err.message);
-  });
+const connection = async () => {
+  await connectToDB();
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+};
+
+connection();
